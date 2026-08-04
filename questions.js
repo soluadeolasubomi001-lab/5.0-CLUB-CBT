7142c9731d636ca3d8d6a4c7d914d6851d8cb9fb
soluadeolasubomi001-lab <soluadeolasubomi001@gmail.com>
2026-08-05 00:33:43 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,42 @@
+const questions = [
+
+{
+    question: "The beautiful girl is ........ the fear of failing her examination.",
+
+    options: [
+        "devoid of",
+        "worried with",
+        "depressed of",
+        "haunted by"
+    ],
+
+    answer: 3
+},
+
+{
+    question: "The wrestler did not ........ during the encounter even when he was exhausted.",
+
+    options: [
+        "give off",
+        "give in",
+        "give up",
+        "give over"
+    ],
+
+    answer: 1
+},
+
+{
+    question: "Choose the option nearest in meaning to the underlined word.\nThe principal rebuked the lazy student.",
+
+    options: [
+        "Praised",
+        "Scolded",
+        "Rewarded",
+        "Ignored"
+    ],
+
+    answer: 1
+}
+
+];
