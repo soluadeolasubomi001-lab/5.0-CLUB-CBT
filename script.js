d6896cdcfb2477c2d9082a2dd946975229d579a1
soluadeolasubomi001-lab <soluadeolasubomi001@gmail.com>
2026-08-07 01:45:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Save the candidate's name
         localStorage.setItem("candidateName", name);
-        const questionCount = document.querySelectorAll("select")[1].value;
         const examTime = document.querySelectorAll("select")[0].value;
 
 localStorage.setItem("examTime", examTime);
-
-localStorage.setItem("questionCount", questionCount);
+        const questionCount = document.querySelectorAll("select")[1].value;
+        );
 
         // Open the exam page
         window.location.href = "exam.html";
